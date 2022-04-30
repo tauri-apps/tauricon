d819dbc306449a85b588d7dc71742f3034d58134
Lucas Nogueira <lucas@tauri.studio>
2022-04-30 10:54:22 -0300

    fix test

--- src/tauricon.ts
+++ src/tauricon.ts
@@ -514,7 +514,7 @@ const tauricon = {
       for (const key in icnsImageList) {
         // eslint-disable-next-line security/detect-object-injection
         const config = icnsImageList[key]
-        const data = await s.resize(config.size).toBuffer()
+        const data = await s.resize(config.size, config.size).toBuffer()
         const image = IcnsImage.fromPNG(data, config.ostype)
         icns.append(image)
       }
