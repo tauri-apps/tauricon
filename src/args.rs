3e96616cc49d28c4c7e6cf2e842a98218fd38863
Juliette Cordor <grassojames5@gmail.com>
2022-05-14 23:35:42 +1000

    added files

---
+++ src/args.rs
@@ -0,0 +1,5 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(about, version, author)]
+pub struct Args {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::Cursor;
+
+use image::io::Reader as ImageReader;
+
+mod args;
+
 fn main() -> anyhow::Result<()> {
     Ok(())
 }
