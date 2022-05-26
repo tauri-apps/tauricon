187bf5a4be55af73bd817e0c8c0cf8e8b92103f6
Juliette Cordor <grassojames5@gmail.com>
2022-05-26 18:57:46 +1000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,51 @@
 use std::io::Cursor;
 
+#[macro_use]
+extern crate log;
+
 use clap::Parser;
 use image::io::Reader as ImageReader;
+use simplelog::*;
 
-mod args;
+#[derive(Parser)]
+#[clap(about, version, author)]
+struct Args {
+    /// The path to the source icon
+    #[clap(default_value = "app-icon.png")]
+    pub icon_path: String,
+
+    /// Target folder
+    #[clap(short, long, default_value = "src-tauri/icons")]
+    pub target: String,
+
+    /// Enable/Disable logging
+    #[clap(short, long)]
+    pub log: bool,
+}
+
+const LEVELFILTER: LevelFilter = {
+    if cfg!(profile = "release") {
+        LevelFilter::Info
+    } else {
+        LevelFilter::Debug
+    }
+};
 
 fn main() -> anyhow::Result<()> {
-    args::Args::parse();
+    let args = Args::parse();
+
+    let logging = args.log;
+
+    CombinedLogger::init(vec![TermLogger::new(
+        if logging {
+            LEVELFILTER
+        } else {
+            LevelFilter::Off
+        },
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )])?;
 
     Ok(())
 }
