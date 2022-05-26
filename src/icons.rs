7cfc421306ce173cc5b6e9595b4f1f15f721f64f
Juliette Cordor <grassojames5@gmail.com>
2022-05-26 20:07:49 +1000

    added icon name parsing

--- src/icons.rs
+++ src/icons.rs
@@ -41,9 +41,38 @@ pub fn parse_icon_path(path: &str) -> anyhow::Result<ImageInfo> {
         .parse::<u32>()
         .context("Failed to parse scale into a number")?;
 
+    let size = &path[..scale_index];
+
+    if size == "icon" {
+        return Ok(ImageInfo {
+            width: 1240,
+            height: 1240,
+            scale,
+            format: image_type,
+        });
+    }
+
+    let (width, height) = {
+        let sizes: Vec<&str> = size.split('x').collect();
+
+        let dimensions = (&sizes[0..=1])
+            .iter()
+            .map(|x| {
+                x.parse::<u32>()
+                    .context("Failed to parse dimension into a number")
+            })
+            .collect::<anyhow::Result<Vec<u32>>>()?;
+
+        if dimensions.len() != 2 {
+            panic!("Juliette you fucked up :)");
+        }
+
+        (dimensions[0], dimensions[1])
+    };
+
     Ok(ImageInfo {
-        width: 0,
-        height: 0,
+        width,
+        height,
         scale,
         format: image_type,
     })
